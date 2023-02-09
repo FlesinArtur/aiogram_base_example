27a533377f29058493558ab790c1712d3dc77595
Artur Flesin <artifloid@gmail.com>
2023-02-10 01:08:46 +0200

    start my telegram bot

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world!")
+from aiogram import Bot, executor, Dispatcher, types
+from settings import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+bot_dispatcher = Dispatcher(bot=bot)
+
+
+@bot_dispatcher.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    await bot.send_message(message.from_user.id, 'I am echo bot, send a message')
+
+
+@bot_dispatcher.message_handler()
+async def new_user_message(message: types.Message):
+    await bot.send_message(message.from_user.id, message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(bot_dispatcher)
